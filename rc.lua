cfa8d1c9bd85ff0b9af92af5526d885d533a2022
Joachim Nilsson <troglobit@gmail.com>
2019-09-14 09:43:13 +0200

    Allow Emacs to be properly maximized
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- rc.lua
+++ rc.lua
@@ -538,6 +538,10 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+
+    { rule = { class = "Emacs" },
+      properties = { size_hints_honor = false }
+    },
 }
 -- }}}
 
