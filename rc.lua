a6609d09ade4c8375f51c9fc64897474ae0684b8
Joachim Nilsson <troglobit@gmail.com>
2019-09-15 10:08:14 +0200

    Import GTK theme as GTK+
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- rc.lua
+++ rc.lua
@@ -62,7 +62,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "gtk/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/themes/gtk+/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 local terminal = "x-terminal-emulator"
