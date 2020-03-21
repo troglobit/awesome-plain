9cb71c537fc42207e1d4f90ae1734571ed1b8e87
Joachim Nilsson <troglobit@gmail.com>
2020-03-21 10:17:21 +0100

    Move window to tag next/prev, while switching to it
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- rc.lua
+++ rc.lua
@@ -466,7 +466,35 @@ clientkeys = gears.table.join(
             c.maximized_horizontal = not c.maximized_horizontal
             c:raise()
         end ,
-        {description = "(un)maximize horizontally", group = "client"})
+        {description = "(un)maximize horizontally", group = "client"}),
+
+    -- https://www.reddit.com/r/awesomewm/comments/7cfef6/how_to_move_client_to_the_next_tag_in_list_tag/
+    awful.key({ modkey, "Shift" }, "Left",
+       function ()
+	  -- get current tag
+	  local t = client.focus and client.focus.first_tag or nil
+	  if t == nil then
+	     return
+	  end
+	  -- get previous tag (modulo 9 excluding 0 to wrap from 1 to 9)
+	  local tag = client.focus.screen.tags[(t.name - 2) % 9 + 1]
+	  awful.client.movetotag(tag)
+	  awful.tag.viewprev()
+       end,
+       {description = "move client to previous tag and switch to it", group = "layout"}),
+    awful.key({ modkey, "Shift" }, "Right",
+       function ()
+	  -- get current tag
+	  local t = client.focus and client.focus.first_tag or nil
+	  if t == nil then
+	     return
+	  end
+	  -- get next tag (modulo 9 excluding 0 to wrap from 9 to 1)
+	  local tag = client.focus.screen.tags[(t.name % 9) + 1]
+	  awful.client.movetotag(tag)
+	  awful.tag.viewnext()
+       end,
+       {description = "move client to next tag and switch to it", group = "layout"})
 )
 
 -- Bind all key numbers to tags.
